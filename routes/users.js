35e6bd8bb893ba58a51739ab004eb3410b68b083
Hamid Alkaff <hamid@Hamids-MacBook-Pro.local>
2019-03-09 21:48:25 +0300

    Register form validation

--- routes/users.js
+++ routes/users.js
@@ -10,4 +10,30 @@ router.get('/register', (req, res) => {
     res.render('users/register');
 });
 
+router.post('/register', (req, res) => {
+    //console.log(req.body);
+    let errors = [];
+
+    if (req.body.password != req.body.password2) {
+        errors.push({text: 'Passwords do not match!'});
+    }
+
+    if (req.body.password.length < 8) {
+        errors.push({text: 'Password must be atleast 8 characters'});
+    }
+
+    if (errors.length > 0) {
+        res.render('users/register', {
+            errors: errors,
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            password2: req.body.password2,
+        });
+    } else {
+        res.send('passed');
+    }
+
+    
+})
 module.exports = router;
